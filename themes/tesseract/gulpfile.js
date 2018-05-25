e9877cfbd939c34ce9b2ec1ef3037e1bce10f01c
Thiago Braga <thibraga06@gmail.com>
2018-05-25 00:38:14 -0300

    Melhorias nos estilos tentando parecer o KB atual do Gitbook

--- themes/tesseract/gulpfile.js
+++ themes/tesseract/gulpfile.js
@@ -11,30 +11,15 @@
 const gulp     = require('gulp'),
   less         = require('gulp-less'),
   autoprefixer = require('gulp-autoprefixer'),
-  rename       = require('gulp-rename'),
   browserSync  = require('browser-sync'),
   reload       = browserSync.reload;
 
-  styles = {
-    base:    './assets/less',
-    src:     './assets/less/theme.less',
-    dest:    './assets/css',
-    options: { outputStyle: 'compressed' }
-  },
-
-  bsFiles = [
-    './assets/css/**/*.css',
-    './layouts/**/*.htm',
-    './pages/**/*.htm',
-    './partials/**/*.htm',
-  ];
-
 // Styles
 gulp.task('styles', function () {
-  return gulp.src(styles.src, { base: styles.base })
-    .pipe(less(styles.options))
+  return gulp.src(['./assets/less/theme.less', './assets/less/vendor.less'])
+    .pipe(less({ outputStyle: 'compressed' }))
     .pipe(autoprefixer('last 2 version'))
-    .pipe(gulp.dest(styles.dest))
+    .pipe(gulp.dest('./assets/css'))
     .pipe(reload({ stream: true }));
 });
 
@@ -43,17 +28,22 @@ gulp.task('browser-sync', function () {
   browserSync.init({
     logPrefix:  'Grupo Tesseract | Knowledge Base',
     host:       'local.kb.grupotesseract.com.br',
-    files:      bsFiles,
     port:       3000,
     open:       false,
     notify:     false,
     ui:         false,
-    logSnippet: false
+    logSnippet: false,
+    files:      [
+      './assets/css/**/*.css',
+      './layouts/**/*.htm',
+      './pages/**/*.htm',
+      './partials/**/*.htm',
+    ]
   });
 });
 
 // Default
 gulp.task('default', ['styles'], function () {
   gulp.start('browser-sync');
-  gulp.watch(styles.src, ['styles']);
+  gulp.watch('./assets/less/**/*.less', ['styles']);
 });
